99211818af22c4aa56b510266f279f9742eae902
quanbruh <hellobrokk27@gmail.com>
2026-02-28 11:28:30 +0700

    close

--- launch.js
+++ launch.js
@@ -280,3 +280,21 @@ document.addEventListener("gameEnded", () => {
   showDestroyMessage();
   createBlackBalloon();
 });
+
+
+// Chặn pinch zoom
+document.addEventListener('touchmove', function (event) {
+  if (event.scale !== 1) {
+    event.preventDefault();
+  }
+}, { passive: false });
+
+// Chặn double-tap zoom
+let lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+  const now = new Date().getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
